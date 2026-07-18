14c2808fee00f43044f3eb9e8fbbcb4cd44c0991
Hrutvik Nagrale <40694467+ihrutvik@users.noreply.github.com>
2026-07-18 14:43:37 +0530

    Add application entry point

---
+++ src/main/java/dev/hrutvik/retryadvisor/RetryAdvisorApplication.java
@@ -0,0 +1,11 @@
+package dev.hrutvik.retryadvisor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetryAdvisorApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RetryAdvisorApplication.class, args);
+    }
+}
